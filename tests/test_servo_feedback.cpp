1022fda713e564e268060e37ef12b0eb00b34350
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 13:12:58 -0400

    Test local servo rudder helper

--- tests/test_servo_feedback.cpp
+++ tests/test_servo_feedback.cpp
@@ -1,25 +1,17 @@
 #include <cassert>
-#include <pypilot_pilots_logic.hpp>
+#include <pypilot_pilots_logic/servo_rudder.hpp>
 
 using namespace pypilot_pilots_logic;
 
 int main() {
-    DataModel disabled;
-    disabled.ap.enabled.value = false;
-    disabled.servo.command_norm.set_internal_command(0.7f, 100);
-    disabled.servo.position_command_deg.set_internal_command(20.0f, 100);
-    PilotsLogic disabled_logic;
-    assert(disabled_logic.compute_command(disabled, 200));
-    assert(disabled.servo.command_norm.value == 0.0f);
-    assert(disabled.servo.position_command_deg.value == 0.0f);
-
-    DataModel faulted;
-    faulted.ap.enabled.value = true;
-    faulted.servo.flags.value = pypilot_data_model::servo_overtemp_fault;
-    PilotsLogic faulted_logic;
-    assert(!faulted_logic.compute_command(faulted, 300));
-    assert(faulted.servo.command_norm.value == 0.0f);
-    assert(faulted.servo.position_command_deg.value == 0.0f);
-
+    assert(!pypilot_servo_faulted(0));
+    assert(pypilot_servo_faulted(pypilot_servo_protocol::OVERTEMP_FAULT));
+    assert(!pypilot_servo_allows_command(pypilot_servo_protocol::OVERTEMP_FAULT));
+    assert(pypilot_rudder_feedback_valid(true, true, 30.0f));
+    assert(!pypilot_rudder_feedback_valid(false, true, 30.0f));
+    assert(!pypilot_rudder_feedback_valid(true, false, 30.0f));
+    assert(!pypilot_rudder_feedback_valid(true, true, 0.0f));
+    assert(pypilot_rudder_position_command(0.5f, 30.0f) == 15.0f);
+    assert(pypilot_reset_command<float>() == 0.0f);
     return 0;
 }
