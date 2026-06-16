336945b9f16074dc0735008ce288781b2ebc9ab2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 01:50:31 -0400

    Add source priority test

---
+++ tests/test_source_priority.cpp
@@ -0,0 +1,4 @@
+#include <cassert>
+#include <pypilot_pilots_logic/source_priority.hpp>
+using namespace pypilot_pilots_logic; using namespace pypilot_data_model;
+int main(){ assert(source_can_update(SensorSource::signalk,SensorSource::serial)); assert(!source_can_update(SensorSource::serial,SensorSource::signalk)); assert(source_can_update(SensorSource::tcp,SensorSource::gpsd)); assert(!source_can_update(SensorSource::gpsd,SensorSource::tcp)); return 0; }
