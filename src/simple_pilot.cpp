897d880173b5e51ef48f048b9486b0b2093410cc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 15:08:48 -0400

    Clear stale position command in simple pilot

--- src/simple_pilot.cpp
+++ src/simple_pilot.cpp
@@ -31,6 +31,7 @@ PilotResult compute_simple_pilot(DataModel& model, uint64_t now_us) {
 
     if (model.ap.enabled.value) {
         model.servo.command_norm.set_internal_command(result.command_norm, now_us);
+        model.servo.position_command_deg.set_internal_command(Real(0), now_us);
     }
 
     return result;
