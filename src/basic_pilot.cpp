4d0b32975c86fc4560d15e3d6561d2ef96240e07
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 10:09:15 -0400

    Update basic compute

--- src/basic_pilot.cpp
+++ src/basic_pilot.cpp
@@ -1,30 +1,37 @@
 #include <pypilot_pilots_logic/basic_pilot.hpp>
-#include <pypilot_pilots_logic/gain.hpp>
-#include <pypilot_pilots_logic/math_helpers.hpp>
+
+#include <pypilot_algorithms/basic_pilot.hpp>
 
 namespace pypilot_pilots_logic {
 
 PilotResult compute_basic_pilot(DataModel& model, uint64_t now_us) {
     PilotResult result;
 
-    Real heading_error = model.ap.heading_error_deg.value;
-    Real heading_rate = model.imu.heading_rate_lowpass_deg_s.value;
-    Real heading_rate_rate = model.imu.heading_rate_rate_lowpass_deg_s2.value;
-    Real command_rate = model.ap.heading_command_rate_deg_s.value;
+    pypilot_algorithms::BasicPilotInput<Real> input;
+    input.heading_error_deg = model.ap.heading_error_deg.value;
+    input.headingrate_deg_s = model.imu.heading_rate_lowpass_deg_s.value;
+    input.headingraterate_deg_s2 = model.imu.heading_rate_rate_lowpass_deg_s2.value;
+    input.heading_command_rate_deg_s = model.ap.heading_command_rate_deg_s.value;
 
-    Real p = compute_gain(heading_error, model.pilots.basic.P.gain.value);
-    Real d = compute_gain(heading_rate, model.pilots.basic.D.gain.value);
-    Real dd = compute_gain(heading_rate_rate, model.pilots.basic.DD.gain.value);
-    Real pr = compute_gain(signed_sqrt_abs(heading_error), model.pilots.basic.PR.gain.value);
-    Real ff = compute_gain(command_rate, model.pilots.basic.FF.gain.value);
+    pypilot_algorithms::BasicPilotGains<Real> gains;
+    gains.P = model.pilots.basic.P.gain.value;
+    gains.D = model.pilots.basic.D.gain.value;
+    gains.DD = model.pilots.basic.DD.gain.value;
+    gains.PR = model.pilots.basic.PR.gain.value;
+    gains.FF = model.pilots.basic.FF.gain.value;
 
-    model.pilots.basic.P.contribution.set(p, now_us);
-    model.pilots.basic.D.contribution.set(d, now_us);
-    model.pilots.basic.DD.contribution.set(dd, now_us);
-    model.pilots.basic.PR.contribution.set(pr, now_us);
-    model.pilots.basic.FF.contribution.set(ff, now_us);
+    pypilot_algorithms::BasicPilotOutput<Real> output =
+        pypilot_algorithms::compute_basic_pilot(input,
+                                                gains,
+                                                pypilot_algorithms::CommandClamp::raw);
 
-    result.command_norm = p + d + dd + pr + ff;
+    model.pilots.basic.P.contribution.set(output.Pgain, now_us);
+    model.pilots.basic.D.contribution.set(output.Dgain, now_us);
+    model.pilots.basic.DD.contribution.set(output.DDgain, now_us);
+    model.pilots.basic.PR.contribution.set(output.PRgain, now_us);
+    model.pilots.basic.FF.contribution.set(output.FFgain, now_us);
+
+    result.command_norm = output.command_norm;
     result.valid = true;
 
     if (model.ap.enabled.value) {
