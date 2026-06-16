759b4af11856ca6bdd2a10cfe460dfe3d7c5f770
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 01:46:06 -0400

    Add source priority API

---
+++ include/pypilot_pilots_logic/source_priority.hpp
@@ -0,0 +1,3 @@
+#pragma once
+#include <pypilot_data_model.hpp>
+namespace pypilot_pilots_logic { bool source_can_update(pypilot_data_model::SensorSource current_source, pypilot_data_model::SensorSource incoming_source); }
