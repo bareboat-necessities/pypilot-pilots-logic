9b6d4683ea0f4a6edf111d84f556e02c5ad19f2d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 01:58:50 -0400

    Format public headers

--- include/pypilot_pilots_logic/gain.hpp
+++ include/pypilot_pilots_logic/gain.hpp
@@ -1,3 +1,10 @@
 #pragma once
+
 #include "types.hpp"
-namespace pypilot_pilots_logic { Real compute_gain(Real input, Real gain); void set_gain_defaults(DataModel& model); }
+
+namespace pypilot_pilots_logic {
+
+Real compute_gain(Real input, Real gain);
+void set_gain_defaults(DataModel& model);
+
+} // namespace pypilot_pilots_logic
--- include/pypilot_pilots_logic/types.hpp
+++ include/pypilot_pilots_logic/types.hpp
@@ -1,8 +1,19 @@
 #pragma once
+
 #include <stdint.h>
+
 #include <pypilot_data_model.hpp>
+
 namespace pypilot_pilots_logic {
+
 using Real = float;
 using DataModel = pypilot_data_model::DataModel<Real>;
-struct PilotResult { bool valid=false; Real command_norm=Real(0); Real position_command_deg=Real(0); bool use_position_command=false; };
-}
+
+struct PilotResult {
+    bool valid = false;
+    Real command_norm = Real(0);
+    Real position_command_deg = Real(0);
+    bool use_position_command = false;
+};
+
+} // namespace pypilot_pilots_logic
