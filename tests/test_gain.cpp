b91ea1a3395c3362e6587972fb1d98246fed4f58
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 13:36:52 -0400

    Use direct algorithm helpers in gain test

--- tests/test_gain.cpp
+++ tests/test_gain.cpp
@@ -2,11 +2,12 @@
 #include <cmath>
 
 #include <pypilot_pilots_logic/gain.hpp>
-#include <pypilot_pilots_logic/math_helpers.hpp>
+#include <pypilot_algorithms/angles.hpp>
+#include <pypilot_algorithms/control.hpp>
 
 int main() {
     assert(std::fabs(pypilot_pilots_logic::compute_gain(10.0f, 0.003f) - 0.03f) < 0.00001f);
-    assert(std::fabs(pypilot_pilots_logic::signed_sqrt_abs(-9.0f) + 3.0f) < 0.00001f);
-    assert(std::fabs(pypilot_pilots_logic::wrap_180(190.0f) + 170.0f) < 0.00001f);
+    assert(std::fabs(pypilot_algorithms::signed_sqrt_abs(-9.0f) + 3.0f) < 0.00001f);
+    assert(std::fabs(pypilot_algorithms::wrap_180_deg(190.0f) + 170.0f) < 0.00001f);
     return 0;
 }
