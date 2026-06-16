f2ba2b66861bd329d69376dcf3a83439e4d2098a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 10:10:45 -0400

    Update simple compute

--- src/simple_pilot.cpp
+++ src/simple_pilot.cpp
@@ -1,21 +1,32 @@
 #include <pypilot_pilots_logic/simple_pilot.hpp>
-#include <pypilot_pilots_logic/gain.hpp>
-#include <pypilot_pilots_logic/math_helpers.hpp>
+
+#include <pypilot_algorithms/simple_pilot.hpp>
 
 namespace pypilot_pilots_logic {
 
 PilotResult compute_simple_pilot(DataModel& model, uint64_t now_us) {
     PilotResult result;
 
-    Real p = compute_gain(model.ap.heading_error_deg.value, model.pilots.simple.P.gain.value);
-    Real i = compute_gain(model.ap.heading_error_int_deg.value, model.pilots.simple.I.gain.value);
-    Real d = compute_gain(model.imu.heading_rate_lowpass_deg_s.value, model.pilots.simple.D.gain.value);
+    pypilot_algorithms::SimplePilotInput<Real> input;
+    input.heading_error_deg = model.ap.heading_error_deg.value;
+    input.heading_error_int_deg = model.ap.heading_error_int_deg.value;
+    input.headingrate_deg_s = model.imu.heading_rate_lowpass_deg_s.value;
 
-    model.pilots.simple.P.contribution.set(p, now_us);
-    model.pilots.simple.I.contribution.set(i, now_us);
-    model.pilots.simple.D.contribution.set(d, now_us);
+    pypilot_algorithms::SimplePilotGains<Real> gains;
+    gains.P = model.pilots.simple.P.gain.value;
+    gains.I = model.pilots.simple.I.gain.value;
+    gains.D = model.pilots.simple.D.gain.value;
 
-    result.command_norm = p + i + d;
+    pypilot_algorithms::SimplePilotOutput<Real> output =
+        pypilot_algorithms::compute_simple_pilot(input,
+                                                 gains,
+                                                 pypilot_algorithms::CommandClamp::raw);
+
+    model.pilots.simple.P.contribution.set(output.Pgain, now_us);
+    model.pilots.simple.I.contribution.set(output.Igain, now_us);
+    model.pilots.simple.D.contribution.set(output.Dgain, now_us);
+
+    result.command_norm = output.command_norm;
     result.valid = true;
 
     if (model.ap.enabled.value) {
