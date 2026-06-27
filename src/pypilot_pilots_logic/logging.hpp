4574ceefa625bc5ee85924ecd6de08c7730ba1b0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-27 01:55:38 -0400

    Update logging include handling

--- src/pypilot_pilots_logic/logging.hpp
+++ src/pypilot_pilots_logic/logging.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
-#include <pypilot_syslib.hpp>
+#if defined(__has_include)
+#  if __has_include(<pypilot_syslib.hpp>)
+#    include <pypilot_syslib.hpp>
+#    define PYPILOT_PILOTS_LOGIC_HAVE_SYSLIB 1
+#  endif
+#endif
+
+#ifndef PYPILOT_PILOTS_LOGIC_HAVE_SYSLIB
+#include <pypilot_algorithms/gps_filter.hpp>
+#endif
 
 namespace pypilot_pilots_logic {
 
