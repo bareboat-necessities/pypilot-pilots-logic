7dbe1f6e35ec64decb73959d6e48199229a0e1c8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-27 01:44:53 -0400

    Add Arduino library implementation unit

---
+++ src/pypilot_pilots_logic_arduino.cpp
@@ -0,0 +1,14 @@
+#ifdef ARDUINO
+
+#include "pypilot_pilots_logic/gain.cpp"
+#include "pypilot_pilots_logic/basic_pilot.cpp"
+#include "pypilot_pilots_logic/absolute_pilot.cpp"
+#include "pypilot_pilots_logic/simple_pilot.cpp"
+#include "pypilot_pilots_logic/rate_pilot.cpp"
+#include "pypilot_pilots_logic/wind_pilot.cpp"
+#include "pypilot_pilots_logic/gps_pilot.cpp"
+#include "pypilot_pilots_logic/vmg_pilot.cpp"
+#include "pypilot_pilots_logic/pilot_select.cpp"
+#include "pypilot_pilots_logic/pilots_logic.cpp"
+
+#endif // ARDUINO
